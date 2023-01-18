1452982bbecd4b27c9cc77e999b1b30d21f913fe
Dmitriy <uplmtq@gmail.com>
2023-01-18 20:53:10 +0300

    Обработчик команды /admin

---
+++ src/tg_er_bot/handlers/admin.py
@@ -0,0 +1,12 @@
+from aiogram import Dispatcher
+from aiogram.types import Message
+
+
+async def admin_start(message: Message):
+    await message.reply("Выберите действие")
+
+
+def register_admin(dp: Dispatcher):
+    dp.register_message_handler(
+        admin_start, commands=["admin"], state="*", is_admin=True,
+    )
