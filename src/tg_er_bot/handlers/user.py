69072595173dd03f1f528cbdad700f85f2778da6
Dmitriy <uplmtq@gmail.com>
2023-01-20 23:43:08 +0300

    Сброс состояний и новое приветствие [/start]

--- src/tg_er_bot/handlers/user.py
+++ src/tg_er_bot/handlers/user.py
@@ -1,15 +1,16 @@
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import CommandStart
 from aiogram.types import Message
 
 from tg_er_bot.services.database import Database
 
 
 async def user_start(message: Message, db: Database, state: FSMContext):
+    await state.reset_state()
     await db.add_user(**message.from_user.values)
-    await message.reply("Hello, user!")
-    # await state.set_state(User.SOME_STATE)
+    await message.reply(f"Привет, {message.from_user.first_name}!")
 
 
 def register_user(dp: Dispatcher):
-    dp.register_message_handler(user_start, commands=["start"], state="*")
+    dp.register_message_handler(user_start, CommandStart(), state="*")
