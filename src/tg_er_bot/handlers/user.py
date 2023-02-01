836eae10442947d38e3e5bd2e0e8989d4cdabc5d
Dmitriy <uplmtq@gmail.com>
2023-02-01 23:04:17 +0300

    Реализация inline-mode

--- src/tg_er_bot/handlers/user.py
+++ src/tg_er_bot/handlers/user.py
@@ -1,7 +1,7 @@
 from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 
 from tg_er_bot.services.database import Database
 
@@ -12,5 +12,22 @@ async def user_start(message: Message, db: Database, state: FSMContext):
     await message.reply(f"Привет, {message.from_user.first_name}!")
 
 
+async def inline_handler(query: InlineQuery, db: Database):
+    currencies = db.get_currencies(query.query)
+
+    articles = [InlineQueryResultArticle(
+        id=currency.ID,
+        title=currency.CharCode,
+        description=f"{currency.Nominal} {currency.Name} = {currency.Value} ₽",
+        input_message_content=InputTextMessageContent(
+            message_text="message_text"
+        )
+    ) for currency in currencies]
+
+    await query.answer(articles, cache_time=60, is_personal=True)
+
+
 def register_user(dp: Dispatcher):
     dp.register_message_handler(user_start, CommandStart(), state="*")
+
+    dp.register_inline_handler(inline_handler, state="*")
